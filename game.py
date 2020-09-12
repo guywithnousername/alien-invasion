eb22d49b21da41f7252670791b0128b133e2ae7f
GoodPositiveKid <mldu@cydu.net>
2020-09-12 12:58:53 -0700

    update game

--- game.py
+++ game.py
@@ -1,20 +1,40 @@
 import sys
 
 import pygame
-
+from settings import Settings
+from ship import Ship
 class game:
   def __init__(self):
     pygame.init()
-
-    self.screen=pygame.displayset_mode((1200,800))
+    self.settings=Settings()
+    self.screen=pygame.display.set_mode((self.settings.width,self.settings.height))
     pygame.display.set_caption('Aliens!')
-
+    self.player = Ship(self)
   def run(self):
     while True:
-      for event in pygame.event.get():
-        if event.type() == pygame.QUIT:
-          sys.exit()
-      pygame.display.flip()
+      self.check()
+      self.update_screen()
+      self.player.update()
+  def check(self):
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        exit()
+      elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_RIGHT:
+            self.player.right=True
+        if event.key == pygame.K_LEFT:
+            self.player.left=True
+        elif event.key == pygame.K_q:
+          exit()
+      elif event.type == pygame.KEYUP:
+        if event.key == pygame.K_RIGHT:
+          self.player.right = False
+        if event.key == pygame.K_LEFT:
+          self.player.left = False
+  def update_screen(self):
+    self.screen.fill(self.settings.bg_color)
+    self.player.show()
+    pygame.display.flip() 
 if __name__ =='__main__':
   ai=game()
-  ai.run_game()
+  ai.run()
--- star.py
+++ star.py
@@ -1,4 +1,16 @@
 import pygame
+import random
 from pygame.sprite import Sprite
 class Star(Sprite):
-  pass
+  def __init__(self,aigame):
+    super().__init__()
+    self.screen = self.aigame
+    self.settings = aigame.settings
+    self.star = pygame.image.load('star.bmp')
+    self.rect = self.star.get_rect()
+    self.rect.midtop = 0
+    self.y = float(self.rect.y)
+  def update(self):
+    self.y += 5
+    self.rect.y = self.y
+
