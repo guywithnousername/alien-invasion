620060a0be74333b82e38f3a0a7b83b3c50f15ae
GoodPositiveKid <mldu@cydu.net>
2020-09-18 14:58:20 -0700

    change bullet color

--- settings.py
+++ settings.py
@@ -8,6 +8,6 @@ class Settings:
     self.bullet_speed = 5.0
     self.bullet_width = 5
     self.bullet_height = 15
-    self.bullet_color = (60,60,60)
+    self.bullet_color = (47, 255, 0)
     choices = (1,2,3)
     self.most_bullets = random.choice(choices)
