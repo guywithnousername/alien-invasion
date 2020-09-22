ab7a705485491cb4eaa2e6d18939ecbe5d7e053d
GoodPositiveKid <69410972+GoodPositiveKid@users.noreply.github.com>
2020-09-21 18:21:11 -0700

    add comments to settings.py

--- games/alien_invasion/settings.py
+++ games/alien_invasion/settings.py
@@ -1,18 +1,20 @@
+'''the file which stores all the settings for the game'''
 import random
 class Settings:
   def __init__(self):
+    #screen settings
     self.width=1000
     self.height=700
     self.bg_color = (25,25,25)
+    #ship settings
     self.speed = 3.0
+    #bullet settings
     self.bullet_speed = 5.0
     self.bullet_width = 15
     self.bullet_height = 10
     self.bullet_color = (0, 255, 242)
-    choices = (1,2,3)
-    self.most_bullets = random.choice(choices)
+    self.most_bullets = 3
+    #alien settings
     self.alien_speed = 1.5
     self.drop_speed = 1
     self.direction = 1 #1 represents right, 2 represents left
-    self.bonus_speed = 3.0
-    self.bonus_direction = -1 #1 represents right, 2 represents left
