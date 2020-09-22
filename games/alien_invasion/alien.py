92aca456b964a2683e81a845c4a326b19e393f03
GoodPositiveKid <69410972+GoodPositiveKid@users.noreply.github.com>
2020-09-22 10:13:06 -0700

    update blue alien

--- games/alien_invasion/alien.py
+++ games/alien_invasion/alien.py
@@ -6,9 +6,9 @@ class Alien(Sprite):
 
   def __init__(self, aigame):
     super().__init__()
-    path = pathlib.Path(__file__).parent.absolute()
+    path = pathlib.Path(__file__).parent.absolute() # get file path
+    self.image = pygame.image.load(str(path) + '/images/alien.bmp') #get location of the alien image
     self.screen= aigame.screen
-    self.image = pygame.image.load(str(path) + '/images/alien.bmp')
     self.rect = self.image.get_rect()
     self.setting = aigame.settings
     self.rect.x = self.rect.width
@@ -16,13 +16,13 @@ class Alien(Sprite):
 
     self.x = float(self.rect.x)
 
-  def check_edges(self):
+  def check_edges(self): #check if touching edge
     screen_rect = self.screen.get_rect()
     if self.rect.right >= screen_rect.right or self.rect.left <= 0:
       return True
 
 
-  def update(self):
+  def update(self): #move the alien
     self.x += (self.setting.alien_speed * self.setting.direction)
     self.rect.x = self.x
 
@@ -32,3 +32,7 @@ class blueAlien(Alien):
     super().__init__(aigame)
     path = pathlib.Path(__file__).parent.absolute()
     self.image = pygame.image.load(str(path) + '/images/blue_alien.bmp')
+    self.rect.x = self.player.rect.x
+    
+  def update(self):
+    pass
