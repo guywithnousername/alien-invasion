3278837bc2884f87a4b775ed141f8678a201ed0c
Minglang Du <mldu@cydu.net>
2020-10-09 09:53:27 -0700

    change color

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Settings:
     self.height=700
     self.actualw=1000
     self.actualh=700
-    self.bg_color = (55, 53, 143)
+    self.bg_color = (104,207,248)
     #ship settings
     self.speed = 3.0
     #bullet settings
